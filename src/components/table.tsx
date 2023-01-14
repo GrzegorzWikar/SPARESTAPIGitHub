6ff370f79811a4a55d9d9bf6695be83b27c25449
GrzegorzWikar <grzegorzwikar938@gmail.com>
2023-01-14 11:06:17 +0100

    File tree 2

---
+++ src/components/table.tsx
@@ -0,0 +1,45 @@
+import { useState } from 'react';
+import { useEffect } from 'react';
+import Table from 'react-bootstrap/Table';
+import CentredModal from './centredModal';
+
+function Tabele () {
+  const [repoData, setRepoData] = useState();
+
+  useEffect(() => {
+    fetch("https://api.github.com/repositories")
+      .then((res) => res.json())
+      .then(
+        (result) => {
+          setRepoData(result)
+        },
+        (error) =>{
+          console.log(error)
+        }
+      )
+  }, [])
+
+  return(
+    <Table striped bordered hover>
+      <thead>
+        <tr>
+          <th>File</th>
+          <th>Description</th>
+          <th>Owner</th>
+        </tr>
+      </thead>
+      <tbody>
+        {repoData?.map((res) => (
+          <tr key={res.id}>
+            <td><a href={res.html_url}>{res.name}</a></td>
+            <td>{res.description}</td>
+            <td><CentredModal repo={res}/></td>
+          </tr>
+        ))}
+      </tbody>
+    </Table>
+  );
+}
+
+
+export default Tabele
